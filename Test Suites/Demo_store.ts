465033a99333d511d1b5d1bfd39adfa3d5473a34
Jules Chendjou <julestl@yahoo.fr>
2022-07-08 15:52:31 -0400

    Add Demo_store

--- Test Suites/Demo_store.ts
+++ Test Suites/Demo_store.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>055e974c-bd9e-4f54-9d61-577891e4b75d</testSuiteGuid>
+   <testCaseLink>
+      <guid>7f09b6f3-a501-48b1-ac8a-42b54d9c94ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/mytestcase</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
